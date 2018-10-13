54cf416574473b3b58f6153cb931643347ffc20d
SzymiJimi <rekas1@tlen.pl>
2018-10-13 21:05:57 +0200

    Initial commit

---
+++ src/main/java/com/thesis/cpmcore/CpmCoreApplication.java
@@ -0,0 +1,12 @@
+package com.thesis.cpmcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmCoreApplication.class, args);
+	}
+}
